a5b7fd3ba8e9e3b6630c57d1993618e7840c3307
Hannele Ruiz <justlemoncl@gmail.com>
2019-05-19 23:04:26 -0400

    Added Cold Resistance placeholder

---
+++ ALOspells/ColdResistance.cs
@@ -0,0 +1,16 @@
+﻿using GTA;
+
+namespace ALOspells
+{
+    /// <summary>
+    /// Cold Resistance
+    /// https://swordartonline.fandom.com/wiki/Cold_resistance_spell
+    /// </summary>
+    public class ColdResistance : Script
+    {
+        public ColdResistance()
+        {
+            // Because Cold Resistance does not makes sense in GTA V, this is a placeholder
+        }
+    }
+}
