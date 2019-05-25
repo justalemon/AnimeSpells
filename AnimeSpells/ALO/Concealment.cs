a5a0ec81ca1671e9c86f702eaa14a75c3e9a4f44
Hannele Ruiz <justlemoncl@gmail.com>
2019-05-25 19:04:32 -0400

    Optimized the ALfheim Online Concealment Spell

--- AnimeSpells/ALO/Concealment.cs
+++ AnimeSpells/ALO/Concealment.cs
@@ -8,9 +8,54 @@ namespace AnimeSpells.ALO
     /// Concealment
     /// https://swordartonline.fandom.com/wiki/Concealment
     /// </summary>
-    public class Concealment : BaseScript
+    public class Concealment : Script
     {
-        public override string Cheat => "concealment";
+        private bool InternalEnabled { get; set; } = false;
+        public bool Enabled
+        {
+            // Return the internal value
+            get => InternalEnabled;
+            set
+            {
+                // Set the internal value
+                InternalEnabled = value;
+                // If the spell has been enabled
+                if (value)
+                {
+                    // if the player is on a vehicle, ignore return
+                    if (Game.Player.Character.CurrentVehicle != null)
+                    {
+                        InternalEnabled = false;
+                        return;
+                    }
+
+                    // Get all of the peds around the player (maybe 50 will get us in trouble, investigate later)
+                    foreach (Ped ped in World.GetNearbyPeds(Game.Player.Character.Position, 50))
+                    {
+                        // If the ped is on combat against the player
+                        if (ped.IsInCombatAgainst(Game.Player.Character))
+                        {
+                            // Return and disable the spell
+                            InternalEnabled = false;
+                            return;
+                        }
+                    }
+
+                    // Set the alpha of the player to zero and hide the weapon
+                    Game.Player.Character.Weapons.Select(WeaponHash.Unarmed);
+                    Game.Player.Character.Alpha = 0;
+                    Function.Call(Hash.SET_POLICE_IGNORE_PLAYER, Game.Player, true); // SET_POLICE_IGNORE_PLAYER
+                    Function.Call(Hash.SET_EVERYONE_IGNORE_PLAYER, Game.Player, true); // SET_EVERYONE_IGNORE_PLAYER
+                }
+                else
+                {
+                    // Reset the alpha and mark as disabled
+                    Game.Player.Character.Alpha = 255;
+                    Function.Call(Hash.SET_POLICE_IGNORE_PLAYER, Game.Player, false); // SET_POLICE_IGNORE_PLAYER
+                    Function.Call(Hash.SET_EVERYONE_IGNORE_PLAYER, Game.Player, false); // SET_EVERYONE_IGNORE_PLAYER
+                }
+            }
+        }
 
         public Concealment()
         {
@@ -19,70 +64,31 @@ namespace AnimeSpells.ALO
             // But all of the other attributes that are carried over from the anime:
             // - The spell is disabled when in contact with other enemies or enemy habilities
             // - You can be Heard, so certain actions like shooting or entering a vehicle will disable the incantation
-
             Tick += OnTick;
         }
 
         private void OnTick(object sender, EventArgs args)
         {
-            // Execute the spell contents on a tick
-            ExecuteSpell();
-        }
-
-        public override void EnableSpell()
-        {
-            // if the player is on a vehicle, return
-            if (Game.Player.Character.CurrentVehicle != null)
+            // If the cheat has been entered
+            if (Tools.HasCheatBeenEntered("concealment") || Tools.HasCheatBeenEntered("con"))
             {
-                return;
+                // Alternate the enabled status
+                Enabled = !Enabled;
             }
 
-            // Get all of the peds around the player (maybe 50 will get us in trouble, investigate later)
-            foreach (Ped ped in World.GetNearbyPeds(Game.Player.Character.Position, 50))
+            // If the spell is enabled
+            if (Enabled)
             {
-                // If the ped is on combat against the player
-                if (ped.IsInCombatAgainst(Game.Player.Character))
+                // Disable the enter vehicle button/key
+                Game.DisableControlThisFrame(0, Control.Enter);
+
+                // If the player has changed the weapon from fists
+                if (Game.Player.Character.Weapons.Current.Hash != WeaponHash.Unarmed)
                 {
-                    // Return, don't activate the spell
-                    return;
+                    // Disable the spell
+                    Enabled = false;
                 }
             }
-
-            // Set the alpha of the player to zero and hide the weapon
-            Game.Player.Character.Weapons.Select(WeaponHash.Unarmed);
-            Game.Player.Character.Alpha = 0;
-            Function.Call(Hash.SET_POLICE_IGNORE_PLAYER, Game.Player, true); // SET_POLICE_IGNORE_PLAYER
-            Function.Call(Hash.SET_EVERYONE_IGNORE_PLAYER, Game.Player, true); // SET_EVERYONE_IGNORE_PLAYER
-            // Mark the spell as enabled
-            Enabled = true;
-        }
-
-        public override void ExecuteSpell()
-        {
-            // If is not enabled, return
-            if (!Enabled)
-            {
-                return;
-            }
-
-            // Disable the enter vehicle button/key
-            Game.DisableControlThisFrame(0, Control.Enter);
-
-            // If the player has changed the weapon from fists
-            if (Game.Player.Character.Weapons.Current.Hash != WeaponHash.Unarmed)
-            {
-                // Disable the spell
-                DisableSpell();
-            }
-        }
-
-        public override void DisableSpell()
-        {
-            // Reset the alpha and mark as disabled
-            Game.Player.Character.Alpha = 255;
-            Function.Call(Hash.SET_POLICE_IGNORE_PLAYER, Game.Player, false); // SET_POLICE_IGNORE_PLAYER
-            Function.Call(Hash.SET_EVERYONE_IGNORE_PLAYER, Game.Player, false); // SET_EVERYONE_IGNORE_PLAYER
-            Enabled = false;
         }
     }
 }
