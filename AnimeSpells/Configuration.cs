9a2d794818446c695ec78e960821dc7ec72de471
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-22 06:54:03 -0400

    Added Mana bar

---
+++ AnimeSpells/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace AnimeSpells
+{
+    public class Configuration
+    {
+        [JsonProperty("mana_x")]
+        public float ManaX { get; set; }
+        [JsonProperty("mana_y")]
+        public float ManaY { get; set; }
+        [JsonProperty("mana_w")]
+        public float ManaWidth { get; set; }
+        [JsonProperty("mana_h")]
+        public float ManaHeight { get; set; }
+        [JsonProperty("mana_offset_x")]
+        public float ManaOffsetX { get; set; }
+        [JsonProperty("mana_offset_y")]
+        public float ManaOffsetY { get; set; }
+        [JsonProperty("mana_offset_w")]
+        public float ManaOffsetWidth { get; set; }
+        [JsonProperty("mana_offset_h")]
+        public float ManaOffsetHeight { get; set; }
+    }
+}
---
+++ AnimeSpells/Manager.cs
@@ -0,0 +1,40 @@
+﻿using GTA;
+using GTA.Native;
+using Newtonsoft.Json;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace AnimeSpells
+{
+    public class Manager : Script
+    {
+        /// <summary>
+        /// The general configuration of the mod.
+        /// </summary>
+        public static Configuration Config { get; } = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("scripts\\AnimeSpells.json"));
+        /// <summary>
+        /// Color for the background of the Mana bar.
+        /// </summary>
+        private readonly Color Background { get; } = Color.FromArgb(220, 0, 0, 0);
+        /// <summary>
+        /// Color for the foreground of the Mana bar.
+        /// </summary>
+        private readonly Color Foreground = Color.FromArgb(255, 73, 149, 182);
+
+        public Manager()
+        {
+            // Over here we subscribe our events
+            Tick += OnTick;
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            // Draw the mana bar
+            // Background
+            Function.Call(Hash.DRAW_RECT, Config.ManaX, Config.ManaY, Config.ManaWidth, Config.ManaHeight, Background.R, Background.G, Background.B, Background.A);
+            // Foreground
+            Function.Call(Hash.DRAW_RECT, Config.ManaX + Config.ManaOffsetX, Config.ManaY + Config.ManaOffsetY, Config.ManaWidth + Config.ManaOffsetWidth, Config.ManaHeight + Config.ManaOffsetHeight, Foreground.R, Foreground.G, Foreground.B, Foreground.A);
+        }
+    }
+}
