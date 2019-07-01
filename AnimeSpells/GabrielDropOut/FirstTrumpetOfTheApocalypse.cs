66466d67d2f1f309b46b7b9aa71e781d5a5b7ec5
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-01 13:04:47 -0400

    Added basics for the Trumpet of the Apocalypse (from Gabriel DropOut)

---
+++ AnimeSpells/GabrielDropOut/FirstTrumpetOfTheApocalypse.cs
@@ -0,0 +1,13 @@
+﻿using GTA;
+
+namespace AnimeSpells.GabrielDropOut
+{
+    /// <summary>
+    /// First Trumpet of the Apocalypse
+    /// Not really a lot of information about it, but it should "Destroy the humanity" according to Gabriel DropOut.
+    /// I ended up using the information of https://en.wikipedia.org/wiki/Seven_trumpets that might not be acurate.
+    /// </summary>
+    public class FirstTrumpetOfTheApocalypse : Script
+    {
+    }
+}
